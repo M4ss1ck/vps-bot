a409583eb817115baac27f03d40c89a411a67992
M4ss1ck <4ndyraul@gmail.com>
2022-06-24 21:31:06 -0400

    add sh command

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,59 @@
 import { Bot } from "grammy";
-import { shellCommand } from "linux-shell-command";
+import { shellCommand, execute } from "linux-shell-command";
 
 const token = process.env.BOT_TOKEN || "";
+const myId = process.env.ADMIN_ID || ""
 const bot = new Bot(token);
 
-bot.command("start", (ctx) => ctx.reply("👀 Nothing here for you... "));
+bot.command("start", (ctx) => {
+  console.log("Launch start command")
+  ctx.reply("👀 Nothing here for you... ")
+});
 
 bot.command("ls", async (ctx) => {
-  try {
-    const sc = shellCommand("ls");
-    await sc
-      .execute()
-      .then((success) => {
+  if (ctx.from?.id === parseInt(myId)) {
+    try {
+      const sc = shellCommand("ls");
+      await sc
+        .execute()
+        .then((success) => {
+          if (success === true) {
+            console.log(sc.stdout);
+            ctx.reply("📁 Listing files...\n" + sc.stdout);
+          } else {
+            console.error(sc.error);
+          }
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    } catch (error) {
+      console.error(error);
+      ctx.reply(`<pre>${JSON.stringify(error)}</pre>`, { parse_mode: "HTML" });
+    }
+  }
+});
+
+bot.command("sh", async (ctx) => {
+  const command = ctx.match
+  // console.log(command)
+  if (ctx.from?.id === parseInt(myId)) {
+    try {
+      await execute(command).then(({ shellCommand: sc, success: success }) => {
         if (success === true) {
           console.log(sc.stdout);
-          ctx.reply("📁 Listing files...");
+          ctx.reply(">> " + command + ":\n" + sc.stdout);
+          sc.kill()
         } else {
           console.error(sc.error);
         }
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  } catch (error) {
-    console.error(error);
-    ctx.reply(`<pre>${JSON.stringify(error)}</pre>`, { parse_mode: "HTML" });
+      }).catch(e => console.log(e))
+    } catch (error) {
+      console.error(error);
+      ctx.reply(`<pre>${JSON.stringify(error)}</pre>`, { parse_mode: "HTML" });
+    }
   }
-});
+})
 
 bot.start();
 console.log("BOT INICIADO");
